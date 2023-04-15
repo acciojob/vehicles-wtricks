00b30a637995fb51c141665c10d9878c90a21352
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-04-15 11:38:27 +0530

    Create Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,6 +11,7 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        
     }
 
     public void changeGear(int newGear){
