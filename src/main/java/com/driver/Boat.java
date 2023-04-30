3835f1fdd536c3dbcd21a6a76b4983ebed0a8e0f
wtricks <webtricks.ak@gmail.com>
2023-04-30 12:03:52 +0530

    Hi

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -26,12 +26,10 @@ public class Boat implements WaterVehicle {
         this.capacity = capacity;
     }
 
-    @Override
     public String getVehicleName() {
         return this.name;
     }
 
-    @Override
     public int getVehicleCapacity() {
         return this.capacity;
     }
